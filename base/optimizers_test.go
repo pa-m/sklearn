073da389e887a7df02c0ba34eb55aaa5dfc329fb
pascal <pascal.masschelier@elbee.fr>
2018-02-27 23:47:16 +0100

    moved adam_test.go code to optimizers_test.go

--- base/optimizers_test.go
+++ base/optimizers_test.go
@@ -58,3 +58,11 @@ func TestAdadelta(t *testing.T) {
 
 	testSolver(t, "Adadelta", s, p)
 }
+
+func TestAdamSolver(t *testing.T) {
+	p := NewRandomLinearProblem(10000, 5, 5)
+	s := NewAdamSolver()
+	// our problem is simplissime with a convex loss function so start with a big StepSize
+	s.StepSize = .5
+	testSolver(t, "AdamSolver", s, p)
+}
