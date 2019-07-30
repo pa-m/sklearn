bf43987e899c57ca0cb38113b616e84e00acd372
Pascal Masschelier <pascal.masschelier@gmail.com>
2019-07-31 00:44:05 +0200

    GP: add log_marginal_likelihood gradient (wip)

--- gaussian_process/gpr.go
+++ gaussian_process/gpr.go
@@ -30,11 +30,11 @@ type GaussianProcessRegressor struct {
 
 // NewGaussianProcessRegressor ...
 func NewGaussianProcessRegressor(kernel kernels.Kernel) *GaussianProcessRegressor {
-	gp:= &GaussianProcessRegressor{
+	gp := &GaussianProcessRegressor{
 		Kernel: kernel,
 		Alpha:  []float64{1e-10},
 	}
-	gp.KernelOpt=gp.Kernel
+	gp.KernelOpt = gp.Kernel
 	return gp
 }
 func (m *GaussianProcessRegressor) IsClassifier() bool { return false }
@@ -61,11 +61,11 @@ func (m *GaussianProcessRegressor) GetNOutputs() int {
 	return m.Ytrain.RawMatrix().Cols
 }
 func (m *GaussianProcessRegressor) Fit(X, Y mat.Matrix) base.Fiter {
-	_,ry := Y.Dims()
+	_, ry := Y.Dims()
 	m.Xtrain = mat.DenseCopyOf(X)
 	m.Ytrain = mat.DenseCopyOf(Y)
-	if len(m.Alpha)!=1 && len(m.Alpha)!=ry {
-		panic(fmt.Errorf("alpha must be a scalar or an array with same number of entries as y.(%d != %d)",len(m.Alpha),ry))
+	if len(m.Alpha) != 1 && len(m.Alpha) != ry {
+		panic(fmt.Errorf("alpha must be a scalar or an array with same number of entries as y.(%d != %d)", len(m.Alpha), ry))
 	}
 	return m
 }
@@ -98,60 +98,66 @@ func (m *GaussianProcessRegressor) LogMarginalLikelihood(Theta mat.Matrix, evalG
 	kernel := m.KernelOpt.CloneWithTheta(Theta)
 	var K *mat.Dense
 	var Kg *tensor.Dense
-	_=Kg
+	_ = Kg
 
 	K, Kg = kernel.Eval(m.Xtrain, nil, evalGradient)
 
 	// add m.Alpha to K diagonal
-	Kraw:=K.RawMatrix()
-	Kdata:=Kraw.Data
-	alphainc:=1
-	if len(m.Alpha)==1 {alphainc=0}
-	nx,_:=m.Xtrain.Dims()
-	for i:=0;i<nx;i++ {
-		Kdata[i*(nx+1)]+=m.Alpha[i*alphainc]
+	Kraw := K.RawMatrix()
+	Kdata := Kraw.Data
+	alphainc := 1
+	if len(m.Alpha) == 1 {
+		alphainc = 0
 	}
-	Ksym:=&mat.SymDense{}
-	Ksym.SetRawSymmetric(blas64.Symmetric{Uplo:blas.Upper,N:Kraw.Rows,Stride:Kraw.Stride,Data:Kraw.Data})
-	cho:=&mat.Cholesky{}
+	nx, _ := m.Xtrain.Dims()
+	for i := 0; i < nx; i++ {
+		Kdata[i*(nx+1)] += m.Alpha[i*alphainc]
+	}
+	Ksym := &mat.SymDense{}
+	Ksym.SetRawSymmetric(blas64.Symmetric{Uplo: blas.Upper, N: Kraw.Rows, Stride: Kraw.Stride, Data: Kraw.Data})
+	cho := &mat.Cholesky{}
 
-	posdef:=cho.Factorize(Ksym)
-	thetarows,_:=Theta.Dims()
+	posdef := cho.Factorize(Ksym)
+	thetarows, _ := Theta.Dims()
 	if evalGradient {
-		grad=make([]float64,thetarows)
+		grad = make([]float64, thetarows)
 	}
 	if !posdef {
-		return math.Inf(-1),grad
+		return math.Inf(-1), grad
 	}
 	// # Support multi-dimensional output of self.y_train_
 	// L = cholesky(K, lower=True)  # Line 2
-	L:=&mat.TriDense{}
+	L := &mat.TriDense{}
 	cho.LTo(L)
 	// alpha = cho_solve((L, True), y_train)  # Line 3
-	alpha:=&mat.Dense{}
-	err:=cho.SolveTo(alpha,m.Ytrain)
+	alpha := &mat.Dense{}
+	err := cho.SolveTo(alpha, m.Ytrain)
 
-	if err!=nil{
+	if err != nil {
 		panic(err)
 	}
 	// log_likelihood_dims = -0.5 * np.einsum("ik,ik->k", y_train, alpha)
 	// log_likelihood_dims -= np.log(np.diag(L)).sum()
 	// log_likelihood_dims -= K.shape[0] / 2 * np.log(2 * np.pi)
-	ry,cy:=m.Ytrain.Dims()
-	lldims:=mat.NewVecDense(cy,nil)
-	tmp:=mat.NewVecDense(cy,nil)
-	for i:=0;i<ry;i++ {
-		tmp.MulElemVec(m.Ytrain.RowView(i),alpha.RowView(i))
-		lldims.AddVec(lldims,tmp)
+	ry, cy := m.Ytrain.Dims()
+	lldims := mat.NewVecDense(cy, nil)
+	tmp := mat.NewVecDense(cy, nil)
+	for i := 0; i < ry; i++ {
+		tmp.MulElemVec(m.Ytrain.RowView(i), alpha.RowView(i))
+		lldims.AddVec(lldims, tmp)
 
 	}
-	sub1:=0.
-	for i:=0;i<ry;i++ {sub1+=math.Log(L.At(i,i))}
-	sub2:=float64(nx)/2*math.Log(2*math.Pi)
-	for i:=0;i<cy;i++ {lldims.SetVec(i,-.5*lldims.AtVec(i)-sub1-sub2)}
+	sub1 := 0.
+	for i := 0; i < ry; i++ {
+		sub1 += math.Log(L.At(i, i))
+	}
+	sub2 := float64(nx) / 2 * math.Log(2*math.Pi)
+	for i := 0; i < cy; i++ {
+		lldims.SetVec(i, -.5*lldims.AtVec(i)-sub1-sub2)
+	}
 
 	// log_likelihood = log_likelihood_dims.sum(-1)  # sum over dimensions
-	lml=mat.Sum(lldims)
+	lml = mat.Sum(lldims)
 	//if eval_gradient:  # compare Equation 5.9 from GPML
 	if evalGradient {
 		// tmp = np.einsum("ik,jk->ijk", alpha, alpha)  # k: output-dimension
@@ -162,28 +168,54 @@ func (m *GaussianProcessRegressor) LogMarginalLikelihood(Theta mat.Matrix, evalG
 		// log_likelihood_gradient_dims = \
 		// 0.5 * np.einsum("ijl,ijk->kl", tmp, K_gradient)
 		// log_likelihood_gradient = log_likelihood_gradient_dims.sum(-1)
-		choSolved:=&mat.Dense{}
-		err=cho.SolveTo(choSolved,eye(nx))
-		if err!=nil {
+		choSolved := &mat.Dense{}
+		err = cho.SolveTo(choSolved, eye(nx))
+		if err != nil {
 			panic(err)
 		}
-		ni,nk:=alpha.Dims()
-		nj:=ni
-		tmp:=tensor.NewDense(tensor.Float64,tensor.Shape{ni,nj,nk})
-		tmpdata:=tmp.Data().([]float64)
-		alpraw:=alpha.RawMatrix()
-		alpdata:=alpraw.Data
-		for i,tmpipos,a1pos:=0,0,0; i<ni; i,tmpipos,a1pos=i+1,tmpipos+nj*nk,a1pos+alpraw.Stride{
+		ni, nk := alpha.Dims()
+		nj := ni
+		tmp := tensor.NewDense(tensor.Float64, tensor.Shape{ni, nj, nk})
+		tmpdata := tmp.Data().([]float64)
+		alpraw := alpha.RawMatrix()
+		alpdata := alpraw.Data
+		for i, tmpipos, a1pos := 0, 0, 0; i < ni; i, tmpipos, a1pos = i+1, tmpipos+nj*nk, a1pos+alpraw.Stride {
 
-			for j,tmpjpos,a2pos:=0,tmpipos,0;j<nj;j,tmpjpos,a2pos=j+1,tmpjpos+nk,a2pos+alpraw.Stride{
-				for k:=0;k<nk;k++{
-					tmpdata[tmpjpos+k]=alpdata[a1pos+k]*alpdata[a2pos+k]-choSolved.At(i,j)
+			for j, tmpjpos, a2pos := 0, tmpipos, 0; j < nj; j, tmpjpos, a2pos = j+1, tmpjpos+nk, a2pos+alpraw.Stride {
+				for k := 0; k < nk; k++ {
+					tmpdata[tmpjpos+k] = alpdata[a1pos+k]*alpdata[a2pos+k] - choSolved.At(i, j)
 				}
 			}
 		}
-		llgdims:= mat.NewVecDense(nk,nil)
-		_=llgdims
-		// TODO ...
+		//# Compute "0.5 * trace(tmp.dot(K_gradient))" without
+		//# constructing the full matrix tmp.dot(K_gradient) since only
+		//# its diagonal is required
+		//log_likelihood_gradient_dims = \
+		//0.5 * np.einsum("ijl,ijk->kl", tmp, K_gradient)
+		tmpShape, tmpStrides := tmp.Shape(), tmp.Strides()
+		KgShape, KgStrides := Kg.Shape(), Kg.Strides()
+		llgdims := mat.NewDense(KgShape[2], tmpShape[2], nil)
+		{
+			Kgd := Kg.Data().([]float64)
+			llgd := llgdims.RawMatrix()
+			for k := 0; k < llgd.Rows; k++ {
+				for l := 0; l < llgd.Cols; l++ {
+					sum := 0.
+					for i, tmpposil, Kgposik := 0, l, k; i < tmpShape[0]; i, tmpposil, Kgposik = i+1, tmpposil+tmpStrides[0], Kgposik+KgStrides[0] {
+						for j, tmpposijl, Kgposijk := 0, tmpposil, Kgposik; j < tmpShape[1]; j, tmpposijl, Kgposijk = j+1, tmpposijl+tmpStrides[1], Kgposijk+KgStrides[1] {
+							sum += tmpdata[tmpposijl] * Kgd[Kgposijk]
+						}
+					}
+					llgdims.Set(k, l, .5*sum)
+				}
+			}
+		}
+		grad = make([]float64, KgShape[2])
+		for i := range grad {
+			grad[i] = mat.Sum(llgdims.RowView(i))
+
+		}
+
 	}
 	return
 }
--- gaussian_process/gpr_test.go
+++ gaussian_process/gpr_test.go
@@ -11,7 +11,7 @@ import (
 
 var _ base.Predicter = &GaussianProcessRegressor{}
 
-func TestGaussianProcessRegressor_Fit(t *testing.T) {
+func TestGaussianProcessRegressor_LogMarginalLikelihood(t *testing.T) {
 	//from plot_gpr_noisy.ipynb
 	//# Instantiate a Gaussian Process model
 	//kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
@@ -24,50 +24,56 @@ func TestGaussianProcessRegressor_Fit(t *testing.T) {
 	//# now the noisy case
 	//X = np.linspace(0.1, 9.9, 20)
 	gp := NewGaussianProcessRegressor(kernel)
-	X:=mat.NewDense(20,1,nil)
+	X := mat.NewDense(20, 1, nil)
 	{
-		x:=X.RawMatrix().Data
-		for i:=range x{x[i]=.1+((9.9-.1)*float64(i)/(float64(len(x)-1)))}
-	}
-	//np.random.seed(1)
-	randomState:=base.NewSource(uint64(1))
-	//dy = 0.5 + 1.0 * np.random.random(y.shape)
-	dy:=make([]float64,20)
-	{
-		for i:=range dy{dy[i]=0.5 + 1.0 * randomState.Float64()}
-	}
-	noise:=make([]float64,20)
-	{
-		for i:=range noise{noise[i]=randomState.NormFloat64()*dy[i]}
-	}
-	Y:=mat.NewDense(20,1,nil)
-	{
-		x:=X.RawMatrix().Data
-		y:=Y.RawMatrix().Data
-		for i:=range y{
-			y[i]=x[i]*math.Sin(x[i])+noise[i]
+		x := X.RawMatrix().Data
+		for i := range x {
+			x[i] = .1 + ((9.9 - .1) * float64(i) / (float64(len(x) - 1)))
 		}
 	}
-	gp.Xtrain=X
-	gp.Ytrain=Y
+	//np.random.seed(1)
+	randomState := base.NewSource(uint64(1))
+	//dy = 0.5 + 1.0 * np.random.random(y.shape)
+	dy := make([]float64, 20)
+	{
+		for i := range dy {
+			dy[i] = 0.5 + 1.0*randomState.Float64()
+		}
+	}
+	noise := make([]float64, 20)
+	{
+		for i := range noise {
+			noise[i] = randomState.NormFloat64() * dy[i]
+		}
+	}
+	Y := mat.NewDense(20, 1, nil)
+	{
+		x := X.RawMatrix().Data
+		y := Y.RawMatrix().Data
+		for i := range y {
+			y[i] = x[i]*math.Sin(x[i]) + noise[i]
+		}
+	}
+	gp.Xtrain = X
+	gp.Ytrain = Y
 
-	expectedTheta:=[]float64{0.,         2.30258509}
-	actualTheta:=mat.DenseCopyOf(gp.Theta()).RawMatrix().Data
-	tol:=1e-3
-	if !floats.EqualApprox(expectedTheta,actualTheta,tol) {
-		t.Errorf("expected theta %g, got %g",expectedTheta,actualTheta)
+	expectedTheta := []float64{0., 2.30258509}
+	actualTheta := mat.DenseCopyOf(gp.Theta()).RawMatrix().Data
+	tol := 1e-3
+	if !floats.EqualApprox(expectedTheta, actualTheta, tol) {
+		t.Errorf("expected theta %g, got %g", expectedTheta, actualTheta)
 	}
 
-	gp.Alpha=[]float64{0.84092936, 1.48919187, 0.25011439, 0.64373756, 0.41829318,
-		0.35086501, 0.47095308, 0.71497294, 0.8041919 , 1.07914021,
+	gp.Alpha = []float64{0.84092936, 1.48919187, 0.25011439, 0.64373756, 0.41829318,
+		0.35086501, 0.47095308, 0.71497294, 0.8041919, 1.07914021,
 		0.84491856, 1.40474526, 0.49625297, 1.89920767, 0.27813767,
-		1.36999419, 0.8414481 , 1.12082415, 0.41009543, 0.48734569}
+		1.36999419, 0.8414481, 1.12082415, 0.41009543, 0.48734569}
 	lml, grad := gp.LogMarginalLikelihood(gp.Theta(), true)
-	expectedLml:=-200.9855428488616
-	if math.Abs(expectedLml-lml)>tol {
+	expectedLml := -200.9855428488616
+	if math.Abs(expectedLml-lml) > tol {
 		t.Errorf("expected lml %g, got %g", expectedLml, lml)
 	}
-	expectedGrad:=[]float64{-0.07469052, -1.52187805}
+	expectedGrad := []float64{-0.07469052, -1.52187805}
 	if !floats.EqualApprox(expectedGrad, grad, tol) {
 		t.Errorf("expected grad %g, got %g", expectedGrad, grad)
 	}
